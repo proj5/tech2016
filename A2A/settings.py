104100468791103d404f7ec1dbfcffc48346170f
Nguyen Duc Nam <net12k44@gmail.com>
2016-05-08 09:19:46 +0700

    Config PostgreSQL for heroku

--- A2A/settings.py
+++ A2A/settings.py
@@ -80,12 +80,18 @@ WSGI_APPLICATION = 'A2A.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if os.environ.get('HEROKU') == '1':
+    import dj_database_url
+    
+    DATABASES = {}
+    DATABASES['default'] = dj_database_url.config(conn_max_age=500)
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 
 
 # Password validation
--- heroku.sh
+++ heroku.sh
@@ -1,5 +1,3 @@
-pip install -r heroku_requirements.txt
-
 python manage.py migrate
 
 gunicorn --pythonpath app A2A.wsgi --log-file -
