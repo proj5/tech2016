713452146dc68de3f36e0da86a4636d26318837d
Nguyen Duc Nam <net12k44@gmail.com>
2016-05-20 15:52:13 +0700

    Add test for answers, comments, questions
    Have 2 fail test for questions
    Update fixture of questions and comments (Add 1 comment to a question)

--- app/answers/tests.py
+++ app/answers/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from answers.models import Answer
+from questions.models import Question
+from a2ausers.models import A2AUser
+
+
+class AnswerTest(TestCase):
+    fixtures = ['auth', 'users', 'topics', 'questions', 'answers']
+
+    def check_num_answers(self):
+        for question in Question.objects.all():
+            self.assertEqual(question.num_answers, question.answers.count())
+
+    def test_add_answer(self):
+        answer = Answer(
+            question=Question.objects.get(pk=1),
+            content='abc123',
+            created_by=A2AUser.objects.get(pk=1),
+            score=5
+        )
+        answer.save()
+        self.check_num_answers()
+
+    def test_delete_answer(self):
+        Answer.objects.get(pk=1).delete()
+        self.check_num_answers()
+
+
+class AnswerApiTest(APITestCase):
+    pass
--- app/comments/tests.py
+++ app/comments/tests.py
@@ -1,3 +1,62 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from comments.models import Comment
+from a2ausers.models import A2AUser
+from questions.models import Question
+from answers.models import Answer
+
+
+class CommentTest(TestCase):
+    fixtures = ['auth', 'users', 'topics', 'questions', 'answers', 'comments']
+
+    def check_num_comments(self):
+        for question in Question.objects.all():
+            self.assertEqual(
+                question.num_comments,
+                Comment.objects.filter(
+                    type='QU',
+                    parent_id=question.pk).count())
+
+    def test_add_comment_to_non_exist_question(self):
+        try:
+            comment = Comment(
+                type='QU',
+                parent_id=10,
+                created_by=A2AUser.objects.get(pk=1)
+            )
+            comment.save()
+        except:
+            return
+
+        self.assertFalse(True, 'Should throw exception')
+
+    def test_add_comment_to_non_exist_answer(self):
+        try:
+            comment = Comment(
+                type='AN',
+                parent_id=10,
+                created_by=A2AUser.objects.get(pk=1)
+            )
+            comment.save()
+        except:
+            return
+
+        self.assertFalse(True, 'Should throw exception')
+
+    def test_add_comment_to_question(self):
+        comment = Comment(
+            type='QU',
+            parent_id=1,
+            created_by=A2AUser.objects.get(pk=1)
+        )
+        comment.save()
+        self.check_num_comments()
+
+    def test_delete_comment_of_question(self):
+        Comment.objects.get(pk=2).delete()
+        self.check_num_comments()
+
+
+class CommentApiTest(APITestCase):
+    pass
