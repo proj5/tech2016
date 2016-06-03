0699664447256530246570e50be754129b4ab7e0
Nguyen Duc Nam <net12k44@gmail.com>
2016-06-03 16:21:14 +0700

    Add test for user api and fix bug

--- app/a2ausers/tests.py
+++ app/a2ausers/tests.py
@@ -1,7 +1,51 @@
 from rest_framework.test import APITestCase
 from rest_framework import status
 
+from a2ausers.models import A2AUser
 # Create your tests here.
+
+
+class AuthenticationApiTest(APITestCase):
+    fixtures = ['auth', 'users']
+
+    def login(self, username, password):
+        url = '/api/v1/auth/login/'
+        data = {'username': username, 'password': password}
+        response = self.client.post(url, data, format='json')
+        return response
+
+    def test_login_success(self):
+        response = self.login('user', 'user1234')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_login_wrong_password(self):
+        response = self.login('user', '123')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_login_no_account(self):
+        response = self.login('123', '123')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_register(self):
+        register_url = '/api/v1/accounts/'
+
+        # Create user and then login
+        data = {
+            'user': {
+                'username': 'abc',
+                'password': 'abc12345',
+                'confirm_password': 'abc12345',
+                'email': 'abc@example.com',
+                'first_name': 'Abc',
+                'last_name': 'Def'
+            }
+        }
+        response = self.client.post(register_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(A2AUser.objects.count(), 3)
+
+        response = self.login('abc', 'abc12345')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 
 class UserAccountApiTest(APITestCase):
@@ -26,46 +70,76 @@ class UserAccountApiTest(APITestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-
-class AuthenticationApiTest(APITestCase):
-    fixtures = ['auth', 'users']
-
-    def test_login_success(self):
-        url = '/api/v1/auth/login/'
-        # Success login
-        data = {'username': 'user', 'password': 'user'}
-        response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_login_wrong_password(self):
-        url = '/api/v1/auth/login/'
-        # Wrong password
-        data = {'username': 'user', 'password': '123'}
-        response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
-    def test_login_no_account(self):
-        url = '/api/v1/auth/login/'
-        # No account
-        data = {'username': 'abc', 'password': 'abc'}
-        response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
-    def test_register(self):
-        register_url = '/api/v1/accounts/'
+    def login(self, username, password):
         login_url = '/api/v1/auth/login/'
+        data = {'username': username, 'password': password}
+        response = self.client.post(login_url, data, format='json')
+        return response
 
-        # Create user and then login
+    def test_update_user_success(self):
+        self.login('admin', 'admin123')
+        url = '/api/v1/accounts/admin/'
+
         data = {
             'user': {
-                'username': 'abc',
-                'password': 'abc',
-                'email': 'abc@example.com'
+                'username': 'admin',
+                'password': 'admin456',
+                'confirm_password': 'admin456',
+                'email': 'abc@example.com',
+                'first_name': 'Abc',
+                'last_name': 'Def'
             }
         }
-        response = self.client.post(register_url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-        data = {'username': 'abc', 'password': 'abc'}
-        response = self.client.post(login_url, data, format='json')
+        response = self.client.put(url, data, format='json')
+        self.client.post('/api/v1/auth/logout/')
+        response = self.login('admin', 'admin456')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        user = A2AUser.objects.get(pk=1)
+        self.assertEqual(user.user.first_name, 'Abc')
+
+    def test_update_user_fail(self):
+        self.login('user', 'user1234')
+        url = '/api/v1/accounts/admin/'
+
+        data = {
+            'user': {
+                'username': 'admin',
+                'password': 'admin456',
+                'confirm_password': 'admin456',
+                'email': 'abc@example.com',
+                'first_name': 'Abc',
+                'last_name': 'Def'
+            }
+        }
+
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        user = A2AUser.objects.get(pk=1)
+        self.assertEqual(user.user.first_name, 'Admin')
+
+    def test_delete_user_success(self):
+        self.login('user', 'user1234')
+        response = self.client.delete('/api/v1/accounts/user/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        self.assertEqual(A2AUser.objects.count(), 1)
+        self.assertFalse(
+            A2AUser.objects.filter(
+                user__username='user').exists())
+
+    def test_delete_user_success(self):
+        self.login('user', 'user1234')
+        response = self.client.delete('/api/v1/accounts/admin/')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        self.assertEqual(A2AUser.objects.count(), 2)
+        self.assertTrue(
+            A2AUser.objects.filter(
+                user__username='admin').exists())
+
+
+class AvatarApiTest():
+    pass
