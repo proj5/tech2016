091c96b8d3bf0e30101af8b009f8dbc7e6ca36cb
Nguyen Duc Nam <net12k44@gmail.com>
2016-06-03 11:38:06 +0700

    Add new test for question

--- app/questions/tests.py
+++ app/questions/tests.py
@@ -15,8 +15,46 @@ class QuestionTest(TestCase):
         for topic in Topic.objects.all():
             self.assertEqual(topic.num_questions, topic.questions.count())
 
+        self.assertEqual(
+            Question.objects.count(),
+            Post.objects.filter(parent__isnull=True).count()
+        )
+
+    def check_num_answers(self):
+        for question in Question.objects.all():
+            self.assertEqual(
+                question.num_answers,
+                Post.objects.filter(parent=question.post).count()
+            )
+
     def test_create_question(self):
-        pass
+        post = Post(
+            content='Testing',
+            created_by=A2AUser.objects.get(pk=1)
+        )
+        post.save()
+
+        question = Question(
+            question='Test',
+            post=post
+        )
+        question.save()
+
+        question.topics.add(Topic.objects.get(pk=1))
+        self.check_num_answers()
+        self.check_num_questions()
+
+    def test_add_answer_to_question(self):
+        post = Post(
+            content='Testing',
+            type='answer',
+            parent=Question.objects.get(pk=1).post,
+            created_by=A2AUser.objects.get(pk=1)
+        )
+        post.save()
+
+        self.check_num_answers()
+        self.check_num_questions()
 
     @unittest.skip('currently fail')
     def test_add_topic_to_question(self):
@@ -36,6 +74,8 @@ class QuestionTest(TestCase):
 
     def test_remove_question(self):
         Question.objects.get(pk=1).delete()
+        self.assertEqual(Question.objects.count(), 1)
+        self.check_num_answers()
         self.check_num_questions()
 
 
