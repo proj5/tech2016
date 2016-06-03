50ae8034cd80f7b8b170cefd21cfdf6f78ae6b57
Xuan Duc Nguyen <nguyenxuanduc.uet@gmail.com>
2016-06-03 14:21:15 +0700

    Add topics API

---
+++ app/topics/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from topics.models import Topic
+
+
+class TopicSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Topic
+        fields = ('id', 'name', 'description')
