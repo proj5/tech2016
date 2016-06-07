64ce9b58d11c7fb6bfe3a8c31ddbd5c69d6a9bd8
Nguyen Duc Nam <net12k44@gmail.com>
2016-06-07 11:04:32 +0700

    Add test for notification's signal

--- app/notifications/tests.py
+++ app/notifications/tests.py
@@ -1,3 +1,154 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
 
+from posts.models import Post, Vote
+from a2ausers.models import A2AUser
+from comments.models import Comment
+from notifications.models import Notification
 # Create your tests here.
+
+
+class NotificationTest(TestCase):
+    fixtures = [
+        'auth', 'users', 'topics', 'posts', 'questions', 'comments'
+    ]
+
+    admin = A2AUser.objects.get(pk=1)
+    user = A2AUser.objects.get(pk=2)
+
+    def get_notifications_per_user(self):
+        result = {}
+        for user in A2AUser.objects.all():
+            result[user] = user.notifications.count()
+        return result
+
+    def get_users_per_notification(self):
+        result = {}
+        for notification in Notification.objects.all():
+            result[notification] = notification.users.count()
+        return result
+
+    def setUp(self):
+        self.notifications_per_user = self.get_notifications_per_user()
+        self.users_per_notification = self.get_users_per_notification()
+
+    def check_notifications_per_user(self, change_user=[]):
+        notifications = self.get_notifications_per_user()
+        for user, num_noti in self.notifications_per_user.iteritems():
+            change_value = 0
+            if user in change_user:
+                change_value = 1
+            self.assertEqual(num_noti + change_value, notifications[user])
+
+    def check_users_per_notification(self):
+        users = self.get_users_per_notification()
+        num_noti_change = 0
+        for noti, num_user in self.users_per_notification.iteritems():
+            if noti in users:
+                self.assertEqual(num_user, users[noti])
+            else:
+                num_noti_change += 1
+
+        self.assertTrue(num_noti_change <= 1)
+
+    def check_notification(self, notification, type, content, users):
+        self.assertEqual(notification.type, type)
+        self.assertEqual(notification.content, content)
+
+        users = set(users)
+        noti_users = set(notification.users.all())
+        self.assertEqual(noti_users, users)
+
+    def test_add_notification_when_add_comment_different_user(self):
+        post = Post.objects.get(pk=1)
+        pre_num_read = self.user.notifications.count()
+        comment = Comment(
+            parent=post,
+            content='Good question',
+            created_by=self.admin
+        )
+        comment.save()
+
+        self.assertEqual(pre_num_read + 1, self.user.notifications.count())
+        self.check_notifications_per_user([self.user])
+        self.check_users_per_notification()
+        self.check_notification(
+            Notification.objects.latest('created_date'),
+            type='01',
+            content='',
+            users=[self.user]
+        )
+
+    def test_add_notification_when_add_comment_same_user(self):
+        post = Post.objects.get(pk=1)
+        pre_num_read = self.user.notifications.count()
+        comment = Comment(
+            parent=post,
+            content='Good question',
+            created_by=self.user
+        )
+        comment.save()
+
+        self.assertEqual(pre_num_read, self.user.notifications.count())
+        self.check_notifications_per_user()
+        self.check_users_per_notification()
+        self.check_notification(
+            Notification.objects.latest('created_date'),
+            type='01',
+            content='',
+            users=[]
+        )
+
+    def test_add_notification_when_add_answer(self):
+        post = Post(
+            type='answer',
+            parent=Post.objects.get(pk=1),
+            content='Ok',
+            created_by=self.admin
+        )
+        post.save()
+
+        self.check_notifications_per_user(post.parent.followed_by.all())
+        self.check_users_per_notification()
+        self.check_notification(
+            Notification.objects.latest('created_date'),
+            type='02',
+            content='',
+            users=post.parent.followed_by.all()
+        )
+
+    def test_add_notification_when_vote_different_user(self):
+        pre_num_read = self.admin.notifications.count()
+        vote = Vote(
+            post=Post.objects.get(pk=2),
+            user=self.user,
+            score=1
+        )
+        vote.save()
+
+        self.assertEqual(pre_num_read + 1, self.admin.notifications.count())
+        self.check_notifications_per_user([self.admin])
+        self.check_users_per_notification()
+        self.check_notification(
+            Notification.objects.latest('created_date'),
+            type='03',
+            content='',
+            users=[self.admin]
+        )
+
+    def test_add_notification_when_vote_same_user(self):
+        pre_num_read = self.admin.notifications.count()
+        vote = Vote(
+            post=Post.objects.get(pk=2),
+            user=self.admin,
+            score=1
+        )
+        vote.save()
+
+        self.assertEqual(pre_num_read, self.admin.notifications.count())
+        self.check_notifications_per_user()
+        self.check_users_per_notification()
+
+
+class NotificationApiTest(TestCase):
+    pass
