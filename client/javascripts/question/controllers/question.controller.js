b9d68312917a0f133f5e91c627bcd087a295c773
iamquang95 <iamquang95@gmail.com>
2016-06-09 13:50:13 +0700

    Finish basic view of question page

--- client/javascripts/question/controllers/question.controller.js
+++ client/javascripts/question/controllers/question.controller.js
@@ -10,16 +10,40 @@
     function QuestionController($scope, $state, $http, $stateParams) {
       var vm = this;
       vm.questionID = $stateParams.questionID;
-      init();
+      getQuestion();
+      getTopics();
+      getAnswers();
 
-      function init() {
+      function getQuestion() {
         //http://localhost:8000/api/v1/question/?questionID=1
-        vm.url = "api/v1/question/?questionID=" + vm.questionID;
-        $http.get(vm.url)
+        var url = "api/v1/question/?questionID=" + vm.questionID;
+        $http.get(url)
         .then(function successCallback(response){
             vm.question = response.data;
         }, function errorCallback(response) {
             console.log("Error get question");
+        });
+      }
+
+      function getTopics() {
+        //http://localhost:8000/api/v1/question/topic/?questionID=1
+        var url = "api/v1/question/topic/?questionID=" + vm.questionID;
+        $http.get(url)
+        .then(function successCallback(response){
+            vm.topics = response.data;
+        }, function errorCallback(response) {
+            console.log("Error get topics");
+        });
+      }
+
+      function getAnswers() {
+        //http://localhost:8000/api/v1/answers/?questionID=1
+        var url = "api/v1/answers/?questionID=" + vm.questionID;
+        $http.get(url)
+        .then(function successCallback(response){
+            vm.answers = response.data;
+        }, function errorCallback(response) {
+            console.log("Error get topics");
         });
       }
     }
