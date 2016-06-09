0e2569808610f9abd6b2b4e7fe4fb0c09e94b648
iamquang95 <iamquang95@gmail.com>
2016-06-09 15:35:30 +0700

    Finish question page

--- client/javascripts/question/controllers/question.controller.js
+++ client/javascripts/question/controllers/question.controller.js
@@ -10,6 +10,8 @@
     function QuestionController($scope, $state, $http, $stateParams) {
       var vm = this;
       vm.questionID = $stateParams.questionID;
+      vm.totalUpvote = 0;
+      vm.totalComment = 0;
       getQuestion();
       getTopics();
       getAnswers();
@@ -20,6 +22,17 @@
         $http.get(url)
         .then(function successCallback(response){
             vm.question = response.data;
+            getRelatedQuestions();
+            vm.totalUpvote += vm.question.post.total_vote;
+            var commentsURL = "/api/v1/comments/id=" + vm.question.post.id + "/"
+            $http.get(commentsURL)
+            .then(function successCallback(response){
+              vm.question.comments = response.data;
+              vm.totalComment += vm.question.comments.length;
+            }, function errorCallback(response) {
+              console.log("Error get comments for question");
+            });
+
         }, function errorCallback(response) {
             console.log("Error get question");
         });
@@ -42,8 +55,30 @@
         $http.get(url)
         .then(function successCallback(response){
             vm.answers = response.data;
+            vm.answers.forEach(function(answer) {
+              vm.totalUpvote += answer.total_vote;
+              var commentsURL = "/api/v1/comments/id=" + answer.id + "/"
+              $http.get(commentsURL)
+              .then(function successCallback(commentResponse){
+                answer.comments = commentResponse.data;
+                vm.totalComment += answer.comments.length;
+              }, function errorCallback(response) {
+                console.log("Error get comments for answer");
+              });
+            });
         }, function errorCallback(response) {
-            console.log("Error get topics");
+            console.log("Error get Answers");
+        });
+      }
+
+      function getRelatedQuestions() {
+        //http://localhost:8000/api/v1/answers/?questionID=1
+        var url = "api/v1/questions/?keyword=" + vm.question.question;
+        $http.get(url)
+        .then(function successCallback(response){
+            vm.relatedQuestions = response.data;
+        }, function errorCallback(response) {
+            console.log("Error get related question");
         });
       }
     }
