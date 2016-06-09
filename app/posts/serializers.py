eed07fcfeeca1e4bb8609ca2914fd9775f0248e9
iamquang95 <iamquang95@gmail.com>
2016-06-09 12:29:42 +0700

    Edit serializer post add user

--- app/posts/serializers.py
+++ app/posts/serializers.py
@@ -1,8 +1,10 @@
 from posts.models import Post, Vote
 from rest_framework import serializers
+from a2ausers.serializers import A2AUserSerializer
 
 
 class PostSerializer(serializers.ModelSerializer):
+    created_by = A2AUserSerializer()
 
     class Meta:
         model = Post
@@ -11,6 +13,7 @@ class PostSerializer(serializers.ModelSerializer):
             'type',
             'content',
             'created_date',
+            'created_by',
             'total_vote'
         )
 
