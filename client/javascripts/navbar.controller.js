5ff6f4cbeb54b9394d38b5fd3ac0852a0c10c832
iamquang95 <iamquang95@gmail.com>
2016-06-10 15:43:38 +0700

    Implement count un-read noti in nav bar

--- client/javascripts/navbar.controller.js
+++ client/javascripts/navbar.controller.js
@@ -5,15 +5,24 @@
     .module('tech2016.controllers')
     .controller('NavBarController', NavBarController);
 
-  NavBarController.$inject = ['$scope', 'Authentication'];
+  NavBarController.$inject = ['$scope', 'Authentication', '$http'];
 
-  function NavBarController($scope, Authentication) {
+  function NavBarController($scope, Authentication, $http) {
     var vm = this;
 
     init();
 
     function init() {
       vm.username = Authentication.getAuthenticatedAccount().username;
+      // http://localhost:8000/api/v1/accounts/admin/
+      var userURL = "api/v1/accounts/" + vm.username + "/";
+      $http.get(userURL)
+      .then(function successCallback(response) {
+          vm.user = response.data;
+        },
+        function errorCallback(response) {
+          console.log("Error when get User")
+        });
     }
 
     vm.logout = function() {
